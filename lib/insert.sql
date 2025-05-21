ca2822856144a66d3be4e2f229eec3a63db9e840
Allan <allanbarongo5@gmail.com>
2025-05-21 19:07:13 +0300

    completed python sql lab on organizing and querying

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',3,'M','black','wild',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',5,'M','brown','rowdy',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tabitha',8,'F','polar','gentle',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',10,'M','black','playful',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',4,'F','polar','aggressive',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',6,'F','brown','grumpy',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',3,'F','polar','protective',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,7,'M','black',NULL,0);
